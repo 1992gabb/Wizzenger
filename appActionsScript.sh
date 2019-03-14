fb894b8e3614a3e08e3690f293aaec496157dec6
Gabriel Bombardier <gabb_bomb@hotmail.com>
2019-03-14 09:48:26 -0400

    Create appActionsScript.sh

---
+++ appActionsScript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+stage('Build') {
+		echo 'Build..'
+		sh './gradlew assembleDebug'
+}
+       
+stage('UI Test') {
+    echo 'Testing..' 
+    //Vérifier si l'app est déja présente sur le device
+		sh '/usr/android-sdk-linux/platform-tools/adb install /var/lib/jenkins/workspace/Wizzenger_Pipeline/app/build/outputs/apk/debug/app-debug.apk'
+		//bat 'C:/Users/gbombardier/AppData/Local/Android/Sdk/tools/emulator.exe -avd "Pixel_2_API_28"'
+		//bat 'gradlew.bat test'
+}
