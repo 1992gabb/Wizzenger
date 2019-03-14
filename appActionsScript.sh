6ad376f8ff039371c1db0e31742207e9c62f92bd
Gabriel Bombardier <gabb_bomb@hotmail.com>
2019-03-14 10:00:54 -0400

    Update appActionsScript.sh

--- appActionsScript.sh
+++ appActionsScript.sh
@@ -2,11 +2,11 @@
 
 #Actions à faire lors du build (builder l'app et le test)
 echo 'Build..'
-sh './gradlew assembleDebug'
+./gradlew assembleDebug
 
 #Phase de test
 echo 'Testing..' 
 
 #Vérifier si l'app est déja présente sur le device
-sh '/usr/android-sdk-linux/platform-tools/adb install /var/lib/jenkins/workspace/Wizzenger_Pipeline/app/build/outputs/apk/debug/app-debug.apk'
+/usr/android-sdk-linux/platform-tools/adb install /var/lib/jenkins/workspace/Wizzenger_Pipeline/app/build/outputs/apk/debug/app-debug.apk
 
