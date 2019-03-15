d86d590eda5352179d497d8c008f7c4a24bbea7c
Gabriel Bombardier <gabb_bomb@hotmail.com>
2019-03-15 12:13:57 -0400

    Update appActionsScript.sh

--- appActionsScript.sh
+++ appActionsScript.sh
@@ -10,7 +10,7 @@ echo 'Building and testing..'
 #wait
 
 result= tail -n1 logErrors.txt | cut -d' ' -f2
-echo BUILD_RESULT= tail -n1 logErrors.txt | cut -d' ' -f2 > play.properties
+echo BUILD_RESULT=result > play.properties
 
 if [ $result=="FAILURE" ]
 then
