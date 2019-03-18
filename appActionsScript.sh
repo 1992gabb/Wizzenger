be88e18d8558ba5c145c4150ca08615bd30ada91
Gabriel Bombardier <gabb_bomb@hotmail.com>
2019-03-18 14:15:19 -0400

    Update appActionsScript.sh

--- appActionsScript.sh
+++ appActionsScript.sh
@@ -28,15 +28,3 @@ else
   echo "Tests succeeded, building new apk."
   ./gradlew assembleDebug
 fi
-
-
-#Si on voulait installer lapp pour lutiliser et non tester
-
-#phone1=1215fcf92c900304
-#phone2=05157df5bd0d0412
-
-#./gradlew assembleDebug
-
-#/usr/android-sdk-linux/platform-tools/adb -s $phone1 install -r  /var/lib/jenkins/workspace/Wizzenger_Pipeline/app/build/outputs/apk/debug/app-debug.apk
-#/usr/android-sdk-linux/platform-tools/adb -s $phone2 install -r  /var/lib/jenkins/workspace/Wizzenger_Pipeline/app/build/outputs/apk/debug/app-debug.apk
-
