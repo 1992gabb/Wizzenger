4374a0102899b156cbc4ff3de6bc6b93ca40d706
Gabriel Bombardier <gabb_bomb@hotmail.com>
2019-03-19 17:09:50 -0400

    Update appActionsScript.sh

--- appActionsScript.sh
+++ appActionsScript.sh
@@ -2,18 +2,18 @@
 
 #Pour certaines fonction, on doit passer l'id du téléphone en paramètre lorsque l'on run le script. 
 
-checkConnectedDevices () {
+checkIfAnyAvdsRunning () {
 	echo "***** check if any avds is running"
-	nbConnectedDevices=$(/usr/android-sdk-linux/platform-tools/adb devices | wc -l) 
-	if [ $nbConnectedDevices == '2' ]; then
-		echo "***** No devices are connected... exiting script..."
+	IsAnyAvdsRunnig=$(adb devices | wc -l) 
+	if [ $IsAnyAvdsRunnig != '2' ]; then
+		echo "*********** No devices are connected, exiting script ***********"
 		exit 1
 	fi
 }
 
 #Pas utilise pour l'instant
 grantPermissions () {
-	echo "***** grant permissions to device $1"
+	echo "*********** grant permissions to device $1 ***********"
 	adb -s $1 shell pm grant com.summit.nexos.sdk.app.service android.permission.WRITE_EXTERNAL_STORAGE
 	adb -s $1 shell pm grant com.summit.nexos.sdk.app.service android.permission.READ_EXTERNAL_STORAGE
 	adb -s $1 shell pm grant com.summit.nexos.sdk.app.service android.permission.READ_PHONE_NUMBERS   
@@ -29,11 +29,11 @@ grantPermissions () {
 
 createFiles(){
 
-    if [ ! -e log.txt ]
+    if [ ! -e logErrors.txt ]
     then
-	echo "Creating necessary files in repo"
+	echo "*********** Creating necessary files in repo ***********"
         sudo touch log.txt
-        sudo chmod 777 log.txt
+        sudo chmod 777 logErrors.txt
         sudo chmod 777 gradlew
         sudo chmod 777 ./app
     fi
@@ -43,31 +43,31 @@ createFiles(){
 launchTests(){
     #/usr/android-sdk-linux/platform-tools/adb uninstall com.bombardier_gabriel.wizzenger.test
     
-	echo "Tests launching on connected devices. Logs will be in log.txt"
-	./gradlew connectedAndroidTest | tee log.txt
+	echo "*********** Tests launching on connected devices. Logs will be in log.txt ***********"
+	./gradlew connectedAndroidTest | tee logErrors.txt
     wait
 }
 
 
 verifyResults(){
 	#Si un test échoue, la ligne suivante est printée : Task :app:connectedDebugAndroidTest FAILED
-    result="$(grep 'Task :app:connectedDebugAndroidTest FAILED' log.txt)" 
+    result="$(grep 'Task :app:connectedDebugAndroidTest FAILED' logErrors.txt)" 
    
 
     if [ "$result" != "" ]
     then
 		#Si failure, on sort du jenkins
-        echo "Test failure, not building new apk."
+        echo "*********** Test failure, not building new apk. ***********"
         exit 0
     else
         #Si fonctionne, on build
-        echo "Tests succeeded, building new apk."
+        echo "*********** Tests succeeded, building new apk. ***********"
         ./gradlew assembleDebug
     fi
 }
 
 
-checkConnectedDevices
+checkIfAnyAvdsRunning
 createFiles
 launchTests
 verifyResults
